eb45e85bd642a053f38b987a29d563846d71ebf6
netospadoni <netospadoni@gmail.com>
2024-10-16 13:58:57 -0400

    aula-1

---
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+// ele vai ignorar tudo o que não foi dito na linha 9
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(@JsonAlias({"Title", "Titulo"}) String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao,
+                         @JsonProperty("imdbVotes") String votos ) {
+}
